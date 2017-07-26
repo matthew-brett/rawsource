0fdd332510c4d8b1dabfac2d11e8ed42d4728bde
Matthew Brett <matthew.brett@gmail.com>
2017-07-26 10:57:05 +0100

    Remove extraneous stuff

--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Script to emit exit code 10 when rawsource is absent
+# For use with git-bisect, to run from sphinx source directory.
+# Clean, reinstall sphinx
 git clean -fxd
 pip install -e .
+# Run test script, emitting error code 0 (success) or 10 (failure)
 MY_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd $MY_DIR
 rm -rf _build
