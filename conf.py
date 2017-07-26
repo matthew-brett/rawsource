f9e3ae572944e3f6f240c3b42b81c65da7363032
Matthew Brett <matthew.brett@gmail.com>
2017-07-26 11:05:40 +0100

    Comments in run_build.sh

--- conf.py
+++ conf.py
@@ -5,5 +5,5 @@ extensions = ['showraw']
 master_doc = 'index'
 # No error code 10 with:
 # html_use_smartypants = False
-# Error code 10 for sphinx >=1.6 with
+# Error code 10 with:
 html_use_smartypants = True
--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Script to emit exit code 10 when rawsource is absent
 # For use with git-bisect, to run from sphinx source directory.
+# Procedure:
+#   git checkout master
+#   git bisect start HEAD 1.5.6 --
+#   git bisect run ~/tmp/rawsource/run_build.sh
+#
+# https://github.com/matthew-brett/rawsource has the code at ~/tmp
+
 # Clean, reinstall sphinx
 git clean -fxd
 pip install -e .
